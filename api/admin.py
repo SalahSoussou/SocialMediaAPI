9fe14cad3d84a37080c90d3ad4cb66f4b75ba6a3
SalahSoussou <soussousalahiddine.com>
2025-01-09 10:49:59 +0100

    feat: add Post, LikePost, and FollowersCount models; register them in admin and update URLs and settings template

--- api/admin.py
+++ api/admin.py
@@ -4,3 +4,6 @@ from django.contrib import admin
 from .models import Profile
 
 admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(LikePost)
+admin.site.register(FollowersCount)
